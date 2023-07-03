1bac96bec6943cb0548cb99b6ccfee2a504dc381
Sergey Zhukov <zhukovsd@gmail.com>
2023-07-03 14:01:01 +0400

    refactor: `Consumer` - autowire `VacancyService` in constructor param

--- src/main/java/com/example/vacancystorageservice/rabbitmq/Consumer.java
+++ src/main/java/com/example/vacancystorageservice/rabbitmq/Consumer.java
@@ -5,7 +5,6 @@ import com.example.vacancystorageservice.service.VacancyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +12,11 @@ public class Consumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
 
-    @Autowired
-    VacancyService vacancyService;
+    final VacancyService vacancyService;
+
+    public Consumer(VacancyService vacancyService) {
+        this.vacancyService = vacancyService;
+    }
 
     @RabbitListener(queues = "${rabbitmq.queue.name}")
     public void consume(VacancyDto vacancyDto){
