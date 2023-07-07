8fa081d8749c6b15c3cdddb459b1d35f9d0eb44b
Sergey Zhukov <zhukovsd@gmail.com>
2023-07-07 14:03:39 +0400

    refactor: `findByQueries` reworked into method without custom `@Query`

--- src/main/java/com/example/vacancystorageservice/repository/VacancyRepository.java
+++ src/main/java/com/example/vacancystorageservice/repository/VacancyRepository.java
@@ -10,7 +10,5 @@ import java.util.List;
 
 @Repository
 public interface VacancyRepository extends MongoRepository<Vacancy, String> {
-
-    @Query("{'queries' :  :#{#query}}")
-    public List<Vacancy> findByQueries(@Param("query") String query);
+    List<Vacancy> findByQueriesContains(String query);
 }
