5eccc6225994cbbf3f559cf252988f6e4c8c64d2
Sergey Zhukov <zhukovsd@gmail.com>
2023-07-10 18:55:06 +0400

    chore: less verbose logging

--- src/main/java/com/example/vacancystorageservice/rabbitmq/Producer.java
+++ src/main/java/com/example/vacancystorageservice/rabbitmq/Producer.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class Producer {
-
     @Value("${rabbitmq.queue.producer.name}")
     private String nameProducerQueue;
 
@@ -20,7 +19,10 @@ public class Producer {
     }
 
     public void send(VacancyDto vacancyDto) {
-            amqpTemplate.convertAndSend(nameProducerQueue, vacancyDto);
-            log.info("Vacancy #" + vacancyDto.getId() + " with query = \"" + vacancyDto.getQuery() + "\" send to RabbitMQ");
-        }
+        amqpTemplate.convertAndSend(nameProducerQueue, vacancyDto);
+        log.debug(
+                "Sending vacancy #" + vacancyDto.getId() + " with query = \"" + vacancyDto.getQuery() + "\" to "
+                + nameProducerQueue + " queue"
+        );
+    }
 }
